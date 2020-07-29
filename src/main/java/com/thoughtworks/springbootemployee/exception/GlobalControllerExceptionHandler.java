1a09c0366c46cde91cb89befa8cf665675d8ec0a
caps <yzsylinder@163.com>
2020-07-29 20:34:18 +0800

    [caps] feature: add GlobalControllerExceptionHandler.java

---
+++ src/main/java/com/thoughtworks/springbootemployee/exception/GlobalControllerExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.springbootemployee.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchCompanyException.class)
+    public void handleNotFound() {
+
+    }
+}
