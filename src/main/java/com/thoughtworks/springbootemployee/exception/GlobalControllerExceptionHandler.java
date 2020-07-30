0592d0083497ef9e5b1f1f823d8f7b5d8a800416
caps <yzsylinder@163.com>
2020-07-30 09:53:32 +0800

    [caps, karol] feature: add response body in globalControllerExceptionHandler.java

--- src/main/java/com/thoughtworks/springbootemployee/exception/GlobalControllerExceptionHandler.java
+++ src/main/java/com/thoughtworks/springbootemployee/exception/GlobalControllerExceptionHandler.java
@@ -1,16 +1,31 @@
 package com.thoughtworks.springbootemployee.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @ControllerAdvice
 public class GlobalControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoSuchCompanyException.class)
-    public void handleNotFound() {
-
+    @ResponseBody
+    public ExceptionStatus handleNotFoundCompany() {
+        ExceptionStatus exceptionStatus = new ExceptionStatus();
+        exceptionStatus.setErrorMessage("No Such Company.");
+        return exceptionStatus;
     }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchEmployeeException.class)
+    @ResponseBody
+    public ExceptionStatus handleNotFoundEmployee() {
+        ExceptionStatus exceptionStatus = new ExceptionStatus();
+        exceptionStatus.setErrorMessage("No Such Employee.");
+        return exceptionStatus;
+    }
+
+//    public ExceptionStatus handleOthers() {
+//        ExceptionStatus exceptionStatus = new ExceptionStatus();
+//        exceptionStatus.setStatus();
+//    }
 }
