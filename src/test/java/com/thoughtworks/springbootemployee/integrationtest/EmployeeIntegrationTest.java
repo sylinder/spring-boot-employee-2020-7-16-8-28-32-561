4550bf9d3ea6b438825d733d744d100fb61d4d0d
caps <yzsylinder@163.com>
2020-08-03 08:31:46 +0800

    [caps, jabin] feature: pass should_return_new_employee_when_update_employee_by_id_given_a_old_employee

--- src/test/java/com/thoughtworks/springbootemployee/integrationtest/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationtest/EmployeeIntegrationTest.java
@@ -138,4 +138,31 @@ public class EmployeeIntegrationTest {
         List<Employee> employeeList = employeeRepository.findAll();
         assertEquals(0, employeeList.size());
     }
+
+    @Test
+    void should_return_new_employee_when_update_employee_by_id_given_a_old_employee() throws Exception {
+        //given
+        Company company = new Company();
+        company.setName("oocl");
+        Company companySaved = companyRepository.save(company);
+        int companyId = companySaved.getId();
+        Employee employee = new Employee();
+        employee.setName("xiaoming");
+        employee.setCompany(companySaved);
+        Employee employeeSaved = employeeRepository.save(employee);
+        int employeeId = employeeSaved.getId();
+        String employeePayload = String.format("{\"id\": %d,\"name\": \"xiaohong\", \"company\": {\"id\": %d}}", employeeId, companyId);
+
+        //when
+        mockMvc.perform(put(String.format("/employees/%d", employeeId))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(employeePayload))
+                .andExpect(status().isOk());
+
+        //then
+        Employee returningEmployee = employeeRepository.findById(employeeId).get();
+        assertEquals("xiaohong", returningEmployee.getName());
+
+    }
+
 }
